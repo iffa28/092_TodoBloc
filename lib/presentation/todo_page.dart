6e76b1c793c70e533c278d322f50e8802b24fa0c
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-05-08 20:10:15 +0700

    add: menambahkan judul halaman dan label selected date

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
 
@@ -9,7 +8,25 @@ class TodoPage extends StatelessWidget {
     final _key = GlobalKey<FormState>();
     final _controller = TextEditingController();
     return Scaffold(
-      
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Todo List'),
+              Row(
+                children: [
+                  Text('Selected Date'),
+
+                ],
+              ),
+              
+
+              
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
