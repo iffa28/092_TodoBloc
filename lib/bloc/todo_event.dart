5befc3519ea54dc94091ac13ac9909a62829f706
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-05-08 19:01:37 +0700

    add: event untuk memilih tanggal

--- lib/bloc/todo_event.dart
+++ lib/bloc/todo_event.dart
@@ -14,7 +14,15 @@ final class TodoEventAdd extends TodoEvent {
 final class TodoEventComplete extends TodoEvent {
   //untuk mengganti status
   final int index;      //untuk membaca array todo
-
-  //constructor untuk membuat event mengganti status todo
+  
+  //constructor untuk membuat event mengganti status
   TodoEventComplete({required this.index});
+}
+
+final class TodoSelectDate extends TodoEvent {
+  // pilih tanggal
+  final DateTime date;  
+
+  //constructor untuk membuat event memilih tanggal
+  TodoSelectDate({required this.date});
 }
