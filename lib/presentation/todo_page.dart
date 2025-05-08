88ba40486c653907ea77dfd3b95a363fd02b695b
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-05-08 20:02:52 +0700

    add: menambahkan stateless widget dengan key untuk validasi dan controller untuk mengambil inputan

---
+++ lib/presentation/todo_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class TodoPage extends StatelessWidget {
+  const TodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
+    return Scaffold(
+      
+    );
+  }
+}
