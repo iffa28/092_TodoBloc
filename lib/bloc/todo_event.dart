3ceb4a5a51cc647a3b69e8604d09625a72476364
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-05-08 18:53:57 +0700

    add: event untuk menambahkan todo sesuai parameternya

--- lib/bloc/todo_event.dart
+++ lib/bloc/todo_event.dart
@@ -2,3 +2,11 @@ part of 'todo_bloc.dart';
 
 sealed class TodoEvent {}
 
+final class TodoEventAdd extends TodoEvent {
+  //untuk menambahkan todo
+  final String title;
+  final DateTime date;
+
+  //constructor untuk membuat event tambah todo
+  TodoEventAdd({required this.title, required this.date});   
+}
