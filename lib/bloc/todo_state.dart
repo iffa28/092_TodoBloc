8f5a1193b03809b04cbd13fc1b882f00801c7252
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-05-08 19:30:27 +0700

    add: menambahkan state untuk menampung data todos dan tanggal yang dipilih

--- lib/bloc/todo_state.dart
+++ lib/bloc/todo_state.dart
@@ -6,3 +6,9 @@ final class TodoInitial extends TodoState {}  //default
 
 final class TodoLoading extends TodoState {}  
 
+final class TodoLoaded extends TodoState {  
+  final List<Todo> todos;  //harus pakai ini supaya seluruh halaman tidak dibaca date
+  DateTime? selectedDate;   //ketika memilih tanggal, harus mengembalikan list, krn selecteddate akan di simpan dalam list
+
+  TodoLoaded({required this.todos, required this.selectedDate});
+}
