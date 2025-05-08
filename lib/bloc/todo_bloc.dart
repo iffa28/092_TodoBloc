d9c50ceb5ce1439312171d6f166afd2c342f777f
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-05-08 19:37:16 +0700

    add: menambahkan logika bisnis untuk menambahkan todo baru

--- lib/bloc/todo_bloc.dart
+++ lib/bloc/todo_bloc.dart
@@ -6,6 +6,22 @@ part 'todo_state.dart';
 
 class TodoBloc extends Bloc<TodoEvent, TodoState> {
   TodoBloc() : super(TodoLoaded(todos: [], selectedDate: null)) {
-    
+    on<TodoEventAdd>((event, emit) {
+      //akan menambahkan data ke list,
+      final currentState = state; //buat variabel untuk menyimpan state
+
+      //cek jika state adalah TodoLoaded
+      if (currentState is TodoLoaded) {
+
+        // duplikat list todos lama agar tidak mengubah aslinya
+        final List<Todo> updatedTodos = List.from(currentState.todos);
+
+        // tambahkan todo baru ke list
+        updatedTodos.add(
+          Todo(title: event.title, isCompleted: false, date: event.date),
+        );
+
+      }
+    });
   }
 }
